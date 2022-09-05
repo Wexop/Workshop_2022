80add82f3565d8ab2a81ebaf4ebe2ace0ccd4d5d
Matthieu <mattheurtin@gmail.com>
2022-09-05 16:05:49 +0200

    start analyse

--- script/fakeNewsAnalyser.py
+++ script/fakeNewsAnalyser.py
@@ -1,10 +1,30 @@
 import requests
 
-url='https://thewebdev.info/2022/04/03/how-to-pass-variables-from-python-flask-to-javascript/'
 
-# in case you need a session
-cd = { 'sessionid': '123..'}
+def getSourceCode(url):
+    cd = {'sessionid': '123..'}
 
-r = requests.get(url, cookies=cd)
-# or without a session: r = requests.get(url)
-print(r.content)
+    r = requests.get(url, cookies=cd)
+    codeSource = r.content
+    return codeSource
+
+
+def fakeNewsAnalyse(url):
+    codeSource = getSourceCode(url)
+
+    fiability = 0
+
+    if UrlAnalyse(url):
+        fiability += 5
+        print("UrlAnalyse : Ok !")
+    else:
+        print("UrlAnalyse : not ok")
+
+    print("FINAL RESULT FIABILITY : ", fiability, "%")
+
+
+def UrlAnalyse(url):
+    return "https" in url
+
+
+fakeNewsAnalyse('https://thewebdev.info/2022/04/03/how-to-pass-variables-from-python-flask-to-javascript/')
