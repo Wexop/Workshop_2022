0708375bf503b9a40cdaeff2f6daffa3b511c7b9
Matthieu <mattheurtin@gmail.com>
2022-09-06 14:55:12 +0200

    Api work

--- api.py
+++ api.py
@@ -1,13 +1,18 @@
+import flask
 from flask import Flask, render_template, request  # pip install flask
 from script import fakeNewsAnalyser
 from script.fakeNewsAnalyser import fakeNewsAnalyse
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/", methods=['GET'])
 def hello():
     url = request.args.get("url")
+    response = flask.jsonify({'some': 'data'})
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return fakeNewsAnalyse(url)
 
 
--- static/fakeNewDetect.js
+++ static/fakeNewDetect.js
@@ -1,5 +1,21 @@
+var callBackGetSuccess =function fakeDetector(data) {
+    console.log(data.main)
+    document.getElementById("test").innerText = data
+}
+
+async function fetchText(url) {
+
+    var url = url
+
+    $.get(url, callBackGetSuccess).done(function () {
+        //alert( "second success" );
+    })
+        .fail(function () {
+            alert("error");
+        })
+        .always(function () {
+            //alert( "finished" );
+        });
 
 
-function fakeDetector() {
-    alert("oui")
 }
