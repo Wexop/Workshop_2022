a5d542b141396010e4d505e1f7406729d7d2565b
Joé <joeaudio1024@gmail.com>
2022-09-06 11:20:55 +0200

    Add author

--- script/fakeNewsAnalyser.py
+++ script/fakeNewsAnalyser.py
@@ -14,20 +14,45 @@ def fakeNewsAnalyse(url):
 
     fiability = 0
     analysePercent = 5
+    authorPercent = 10
 
     if UrlAnalyse(url):
         fiability += analysePercent
+    fiability += AuthorAnalyse(codeSource) * authorPercent
+
 
     print("FINAL RESULT FIABILITY : ", fiability, "%")
 
 
 def UrlAnalyse(url):
-
     if "https" in url:
         print("UrlAnalyse : Ok !")
         return True
     else:
         print("UrlAnalyse : not ok")
+
+
+def AuthorAnalyse(code):
+    tab = str(code).split("<")
+    author = False
+    href = False
+    fiabilty = 0
+
+    for i in range(len(tab)):
+        test = tab[i].lower()
+        if "author" in str(test):
+            if "author" in tab[i]:
+                author = True
+            if "author" in tab[i] and "href" in tab[i]:
+                href = True
+    if author:
+        fiabilty += 0.5
+    if href:
+        fiabilty += 0.5
+
+    return fiabilty
+
+
 
 
 fakeNewsAnalyse('https://thewebdev.info/2022/04/03/how-to-pass-variables-from-python-flask-to-javascript/')
