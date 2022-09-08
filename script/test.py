42c5ced236abe11913a7e30ad61b7df472acacbc
Matthieu <mattheurtin@gmail.com>
2022-09-08 14:43:30 +0200

    fonction de comparaison des textes
    # Conflicts:
    #	script/fakeNewsAnalyser.py

--- script/test.py
+++ script/test.py
@@ -1,3 +1,37 @@
-t = "Je <div class='vedbe'> zevzev"
-t = t.split("v", 1)[1]
-print(t)
+import regex
+import csv
+
+t = ["Je <div class='vedbe'> \n\nzevzev"]
+
+def writeTxt(nom, sourceCode):
+    file = open(nom + ".txt", "w+")
+    file.write(sourceCode)
+    file.close()
+
+def readtxt(nom):
+    file = open(nom + ".txt", "r")
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+        contenu = line.split()
+    return contenu
+
+def delAntiSlash(contenu):
+    n = 0
+    for element in contenu:
+        print(element)
+        for char in element:
+            print(char)
+            while len(element) < n:
+                if char[n] + char[n+1] == '\\':
+                    contenu.pop(element.index())
+            n+=1
+    return contenu
+
+def MotsClés(listeMots):
+    for mot in listeMots:
+        print(len(mot))
+
+print(readtxt("test"))
+MotsClés(readtxt("test"))
+
