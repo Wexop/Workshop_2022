a545f9f4c7d37a58da5e2f5c9965973e0acb8205
Matthieu <mattheurtin@gmail.com>
2022-09-08 15:19:27 +0200

    Trad french

--- static/fakeNewDetect.js
+++ static/fakeNewDetect.js
@@ -1,10 +1,10 @@
 var callBackGetSuccess =function fakeDetector(data) {
     console.log(data)
     document.getElementById("text").innerText = data.fiability + " fiable !"
-    document.getElementById("linkIsSafe").innerText = "Link is safe : " + data.info.urlIsSafe
-    document.getElementById("authorFound").innerText = "Author found : " + data.info.authorFound
-    document.getElementById("authorLink").innerText = "Author have a link : " + data.info.authorLink
-    document.getElementById("subjectFound").innerText = "Subject found : " + data.info.subjectFound
+    document.getElementById("linkIsSafe").innerText = "Le lien est sécurisé : " + (data.info.urlIsSafe ? "Oui !" : "non")
+    document.getElementById("authorFound").innerText = "Un auteur à été trouvé  : " + (data.info.authorFound ? "Oui !" : "non")
+    document.getElementById("authorLink").innerText = "L'auteur possède une page web : " + (data.info.authorLink ? "Oui !" : "non")
+    document.getElementById("subjectFound").innerText = "Sujet trouvé : " + data.info.subjectFound
 
 
     const divContent = []
@@ -13,7 +13,7 @@ var callBackGetSuccess =function fakeDetector(data) {
         const balise = '<a class="link" target="_blank" href= "' + data.info.webLink[i] + '" > lien ' + (i+1) +  ' : ' + data.info.webLink[i] +' </a>'
         divContent.push(balise)
     }
-    document.getElementById("allLinks").innerHTML = "Link related : "
+    document.getElementById("allLinks").innerHTML = "Liens trouvé sur des sites fiables en rapport avec l'article : "
     document.getElementById("allLinks").innerHTML += '</br>'
     for(let i = 0; i < divContent.length; i++){
         document.getElementById("allLinks").innerHTML += divContent[i] + '<hr>'
