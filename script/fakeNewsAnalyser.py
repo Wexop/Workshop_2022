74d6bdcc4599070d04d9ad134bdf44ef210e33e1
Saayli <hessabilyas@gmail.com>
2022-09-08 12:57:18 +0200

    fonction de comparaison des textes

--- script/fakeNewsAnalyser.py
+++ script/fakeNewsAnalyser.py
@@ -1,5 +1,5 @@
 import requests
-
+import regex
 
 def getSourceCode(url):
     cd = {'sessionid': '123..'}
@@ -33,7 +33,7 @@ def UrlAnalyse(url):
     else:
         print("UrlAnalyse : not ok")
 
-def SupprHeaderFooter(url):
+def delHeaderFooter(url):
     sourceCode = str(getSourceCode(url))
     sourceCode = sourceCode.split("</header>")
     sourceCode = str(sourceCode[-1])
@@ -41,17 +41,59 @@ def SupprHeaderFooter(url):
     sourceCode = str(sourceCode[0])
     return sourceCode
 
-def supprBalise(sourceCode):
-    tmp = ""
-    for char in sourceCode:
-        if char == "<":
-            sourceCodeTmp = sourceCode.split("<", 1)[1]
-            while sourceCodeTmp[0] != ">":
-                for tmp in sourceCodeTmp:
-                    sourceCodeTmp.remove
+def delBalise(sourceCode):
+     sourceCode = regex.sub('<.+?>', '', sourceCode)
+     return sourceCode
+
+def writeTxt(nom, sourceCode):
+    file = open(nom + ".txt", "w+")
+    file.write(sourceCode)
+    file.close()
+
+def readtxt(nom):
+    file = open(nom + ".txt", "r")
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+        contenu = line.split()
+    return contenu
+
+sourcecode = delHeaderFooter("https://thewebdev.info/2022/04/03/how-to-pass-variables-from-python-flask-to-javascript/")
+writeTxt("sourcecode1", delBalise(sourcecode))
+listeMots = readtxt("sourcecode1")
+
+def MotsClés(listeMots):
+    motsClés = []
+    for mot in listeMots:
+        if len(mot) > 4 and len(mot) < 15:
+            motsClés.append(mot)
+    return motsClés
+
+def comparaisonTexte(Url1, Url2 ):
+    sourceCode1 = delBalise(delHeaderFooter(Url1))
+    sourceCode2 = delBalise(delHeaderFooter(Url2))
+    writeTxt("sourcecode1", delBalise(sourceCode1))
+    writeTxt("sourcecode2", delBalise(sourceCode2))
+    listeMots1 = readtxt("sourcecode1")
+    listeMots2 = readtxt("sourcecode2")
+    listeMots1 = MotsClés(listeMots1)
+    listeMots2 = MotsClés(listeMots2)
+    cpt = 0
+    for mot in listeMots1:
+        for mot2 in listeMots2:
+            if mot == mot2:
+                cpt += 1
+    reponse = "Le premier texte de l'article contient {len1} mots et le deuxième contient {len2} mots. Il y a {cpt} itérations de mots clés.".format(len1 = len(listeMots1), len2 = len(listeMots2), cpt = cpt)
+    return reponse
+
+print(comparaisonTexte("https://www.francetvinfo.fr/monde/europe/manifestations-en-ukraine/direct-guerre-en-ukraine-l-onu-juge-credibles-les-accusations-d-enfants-transferes-de-force-en-russie-moscou-denonce-une-legende_5349115.html", "https://www.20minutes.fr/monde/3347107-20220907-guerre-ukraine-direct-malgre-rapport-inquietant-aiea-tirs-visent-encore-centrale-zaporojie"))
 
 
-print(SupprHeaderFooter("https://thewebdev.info/2022/04/03/how-to-pass-variables-from-python-flask-to-javascript/"))
+
+
+
+
+
 
 
 
